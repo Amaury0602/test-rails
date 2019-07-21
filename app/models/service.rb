3eed19983ecef76da2e1d075e860863c9e5959ef
amaury ropert <amauryropert@gmail.com>
2019-07-21 11:58:11 +0200

    creation table services + qq seeds

---
+++ app/models/service.rb
@@ -0,0 +1,5 @@
+class Service < ApplicationRecord
+  validates :name, :description, presence: true
+  validates :price, presence: true
+  validates :price, numericality: { greater_than: 0 }
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Service.destroy_all
+
+Service.create(name: "Laver le linge", description: "Le linge est sale il faut le laver", price: 7)
+Service.create(name: "Manger", description: "Mnager des pates au resto", price: 10)
+Service.create(name: "Promener le chien", description: "Le chien doit faire ses besoins promène le", price: 5)
+Service.create(name: "Monter la commode", description: "La voisine a du mal à monter sa commode", price: 20)
+Service.create(name: "Fermer sa gueule", description: "Tu énerves tout le monde tais-toi", price: 1)
+Service.create(name: "Aller au boulot", description: "Tu devrais songer à gagner ta vie", price: 2000)
